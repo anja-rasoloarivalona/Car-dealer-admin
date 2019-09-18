4c9f788c33fbfba05e6ecf6553d36d51a9b1cd42
anja-rasoloarivalona <rasoloanja@gmail.com>
2019-09-18 18:20:46 -0400

    Init FormInput component

---
+++ src/components/formInput/FormInput.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import './FormInput.css';
+
+const formInput = props => {
+    return (
+        <div className="input">
+
+            {/*--------INPUT LABEL-----------*/}
+            {
+                props.label && (
+                    <label htmlFor={props.id}>{props.label}</label>
+                )
+            }
+
+            {/*-------INPUT VARIATION--------*/}
+
+
+                {/*-----BASIC INPUT--------*/}
+                {
+                    props.control === 'input' && (
+                        <input className="input__field" 
+                               type={props.type} //text, email, password...
+                               id={props.id}
+                               required={props.required}
+                               value={props.value}
+                               placeholder={props.placeholder}
+                               onChange={e => props.onChange(props.id, e.target.value)}
+                               />
+                    )
+                }
+
+
+
+                {/*----TEXTAREA INPUT--------*/}
+                {
+                    props.control === 'textarea' && (
+                        <textarea className="textarea"
+                            id={props.id}
+                            required={props.required}
+                            value={props.value}
+                            onChange={e => props.onChange(props.id, e.target.value)}
+                            />
+                    )
+                }
+
+                {
+                    props.control === 'select' && (
+                        <select id={props.id}
+                                required={props.required}
+                                value={props.value}
+                                onChange={e => props.onChange(props.id, e.target.value)}>
+                                
+                                <option value="option1">Option1</option>
+                                <option value="option2">Option2</option>
+                                <option value="option3">Option3</option>
+                        </select>
+                    )
+                }
+        </div>
+    )
+}
+
+export default formInput;
