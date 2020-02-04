6b56d9649a516cda6d2ffae5a439b2a5384b2c42
anja <rasoloanja@gmail.com>
2020-02-04 13:37:24 -0500

    Added logic to allow the admin to search for users

--- src/pages/users/Users.js
+++ src/pages/users/Users.js
@@ -2,99 +2,190 @@ import React, { Component } from 'react';
 import './Users.css';
 import IconSvg from '../../utilities/svg/svg';
 import openSocket from 'socket.io-client';
+import Spinner from '../../components/spinner/Spinner';
+
+
 
  class Users extends Component {
 
     state = {
         users: [],
+        searchedUser: '', 
+        scrollPos: 0,
+        searchedUserResults: [],
+        searchedUserNoResults: false,
+        searchingUser: false,
+    }
+
+    componentWillMount(){
+        this.timer = null
+    }
+
+    componentWillUnmount(){
+        this.timer = null
     }
 
     componentDidMount(){
-
-       let url = "http://localhost:8000/admin/users";
-       let method = "GET";
-
-       fetch(url, {
-           headers: {
-               "Content-Type": "application/json"
-           },
-           method: method
-       })
-       .then(res => {
-            if (res.status !== 200) {
-            throw new Error("Failed to fetch users");
-            }
-            return res.json(); //extract the body
-        })
-        .then(resData => {
-
-            this.setState({ users: resData.users},
-            () => console.log('initial state', this.state.users))
-        })
-        .catch(err => {
-            console.log(err);
-        });
-
+        this.fetchUsersdata();
         const socket = openSocket('http://localhost:8000');
-
         socket.on('userLoggedIn', data => {
             let userLoggedInId = data._id;
-
             const userLoggedIn = this.state.users.find( i => i._id === userLoggedInId);
-
             userLoggedIn.active = true;
-
             userLoggedIn.connection = [data.connection[data.connection.length - 1]];
-
             const newUserStates = this.state.users.filter(i => i._id !== userLoggedInId);
             let newData = [];
-
             if(newUserStates.length === 0){
                 newData = [userLoggedIn];
-                this.setState({ users: newData} , () => console.log('state', this.state.users))
+                this.setState({ users: newData})
             } else {
                 newData = [userLoggedIn, ...newUserStates]
-                this.setState({ users: newData}, () => console.log('state', this.state.users))
+                this.setState({ users: newData})
             }
         })
-
         socket.on('userLoggedOut', data => {
-
-            console.log('logged out happening')
             let userLoggedInId = data._id;
-
-
             let userLoggedInIndex = this.state.users.findIndex( i => i._id === userLoggedInId)
-
             const userLoggedIn = this.state.users.find( i => i._id === userLoggedInId);
-
             userLoggedIn.active = false;
-
             userLoggedIn.connection = [data.connection[data.connection.length - 1]];
-
             let newUsersData = [...this.state.users];
             newUsersData[userLoggedInIndex] = userLoggedIn;
-
-
             this.setState({ users: newUsersData})
+        })        
+    }
+
+
+    fetchUsersdata = () => {
+        let url = "http://localhost:8000/admin/users";
+        let method = "GET";
+        fetch(url, {
+            headers: {
+                "Content-Type": "application/json"
+            },
+            method: method
         })
+        .then(res => {
+             if (res.status !== 200) {
+             throw new Error("Failed to fetch users");
+             }
+             return res.json(); //extract the body
+         })
+         .then(resData => {
+             this.setState({ users: resData.users})
+         })
+         .catch(err => {
+             console.log(err);
+         });
+    }
 
+    searchUserHandler = value => {
+        clearTimeout(this.timer);
+        this.setState({searchedUser: value, searchedUserNoResults: false });
+        this.timer = setTimeout( () => this.fetchSearchedUsers(), 1000) 
+    }
 
+    fetchSearchedUsers = () => {
+        let url = 'http://localhost:8000/user/search-user';
+        let method = 'POST'
 
+        if(this.state.searchedUser !== ''){
+            this.setState({ searchingUser: true})
+            fetch(url, {
+                method: method,
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify( { search : this.state.searchedUser})
+            })
+            .then( res => {
+                if(res.status === 422){
+                    throw new Error (
+                        "Make sure the email isn't used yet"
+                    )
+                }
+    
+                if(res.status !== 200 && res.status !== 201){
+                    throw new Error ( ' Creating a user failed')
+                }
+                return res.json()
+            })
+            .then(resData => {
+                let results = resData.users;
+                if(results.length > 0){
+                    this.setState({searchedUserResults:  resData.users, searchingUser: false })
+                } else {
+                    this.setState({searchedUserResults:  resData.users, searchingUser: false, searchedUserNoResults: true })
+                }
+                
+                console.log('useeers', resData.users)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+            } else {
+                this.setState({ searchedUserResults: [], searchedUserNoResults: false})
+            }
+        }
+        
 
-
-
+    scrollHandler = () => {
+        let scrollPos = window.pageYOffset;
+        this.setState({ scrollPos})
     }
 
 
     render() {
 
-        const {users} = this.state;
+        const {users, searchedUser, scrollPos, searchedUserResults, searchedUserNoResults, searchingUser } = this.state;
 
+        let inputIcon = <IconSvg icon="search"/>
+        if(searchingUser){
+            inputIcon = <Spinner />
+        }
         return (
             <section className="users">
+
+                <div className="users__search">
+                    <div className="users__search__inputContainer">
+                        <input className="users__search__input" 
+                                    type='text'
+                                    value={searchedUser}
+                                    placeholder='Name Filter'
+                                    onChange={e => this.searchUserHandler(e.target.value)}
+                        />
+                        {inputIcon}
+                    </div>
+                    
+                    
+                    
+                   
+
+                    {searchedUserResults.length > 0 && (
+                        <ul className="users__search__results">
+
+
+                            {searchedUserResults.map(user => (
+                                <li className="users__search__results__item">
+                                    <div className="users__search__results__item__avatar">
+                                        {user.firstName.slice(0, 1)}{user.lastName.slice(0, 1)}
+                                    </div>
+                                    <div className="users__search__results__item__name">
+                                        {user.firstName} {user.lastName}
+                                    </div>
+                                </li>
+                            ))}
+
+                            
+                        </ul>
+                    )}
+
+                    
+                </div>
                 <table className="users__table">
-                    <thead>
+                    <thead className={ `users__table__header 
+                                        ${ scrollPos > 90 ? 'fixed' : ''}`
+                            }>
                         <tr>
                             <th>&nbsp;</th>
                             <th>Identity</th>
@@ -130,7 +221,7 @@ import openSocket from 'socket.io-client';
                                 </td>
                                 <td className="users__table__data__info">
                                     <div>
-                                        +1 438 896 2026
+                                        {user.phoneNumber ? user.phoneNumber : 'N.D.'}
                                     </div>
                                 </td>
                                 <td className="users__table__data__status">
@@ -144,7 +235,9 @@ import openSocket from 'socket.io-client';
                                 <td className="users__table__data__info">
                                     <div>
                                         {
-                                            user.active === true ?  user.connection[0].start : user.connection[0].end
+                                            user.active === true ?  user.connection[0].start : 
+                                            
+                                            user.connection[0] ? user.connection[0].end : 'N.D.'
                                         }
                                     </div>
                                     
