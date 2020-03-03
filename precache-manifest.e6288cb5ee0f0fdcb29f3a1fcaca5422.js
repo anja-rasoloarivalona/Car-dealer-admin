ac2136ff19c35b3dbe8cf5410e5fbdcf43251a9b
anja <rasoloanja@gmail.com>
2020-03-02 19:54:34 -0500

    Updates

---
+++ precache-manifest.e6288cb5ee0f0fdcb29f3a1fcaca5422.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46a8e68ab0ac7eeb9a5c7e7d9316031f",
+    "url": "/woto-motors-admin/index.html"
+  },
+  {
+    "revision": "cdd1b8fe8b227d025a0f",
+    "url": "/woto-motors-admin/static/css/2.564e90a8.chunk.css"
+  },
+  {
+    "revision": "e7abd429eba969b0be92",
+    "url": "/woto-motors-admin/static/css/main.dffcb98b.chunk.css"
+  },
+  {
+    "revision": "cdd1b8fe8b227d025a0f",
+    "url": "/woto-motors-admin/static/js/2.4e07cf7f.chunk.js"
+  },
+  {
+    "revision": "b2aef2d316188a16e8626768b912ae73",
+    "url": "/woto-motors-admin/static/js/2.4e07cf7f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e7abd429eba969b0be92",
+    "url": "/woto-motors-admin/static/js/main.82bc3147.chunk.js"
+  },
+  {
+    "revision": "ab94f4d0180d53d0a612",
+    "url": "/woto-motors-admin/static/js/runtime-main.124af6f3.js"
+  },
+  {
+    "revision": "4b55d60c83fc9fb0d6b962dd2785cd6b",
+    "url": "/woto-motors-admin/static/media/desert-suv.4b55d60c.jpg"
+  },
+  {
+    "revision": "b5e7acadf2f28537c01f00da3611a794",
+    "url": "/woto-motors-admin/static/media/eventually.b5e7acad.mp3"
+  },
+  {
+    "revision": "737c6dbb84ffe30e661fcb3fff8a8c45",
+    "url": "/woto-motors-admin/static/media/sprite.737c6dbb.svg"
+  }
+]);
