a6c42d471597d34ed5e884cd49f73ef2dea549cc
anja <rasoloanja@gmail.com>
2020-03-02 20:23:17 -0500

    Fixed routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
@@ -34,9 +34,9 @@ const store = createStore(
 )
 ReactDOM.render(
     <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </Provider>
     
 , 
